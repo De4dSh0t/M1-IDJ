ffc6acf8060bb9766b2c2993cc7d4a9fe1fecdcf
De4dSh0t <ruipsilva2001@hotmail.com>
2020-04-21 20:14:36 +0100

    QuestSystem v0.2 (Added "Reward")

---
+++ QuestSystem/QuestSystem/Reward.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace QuestSystem
+{
+    public class Reward
+    {
+        private float quantity; //Representa a quantidade da recompensa
+
+        public float Quantity
+        {
+            get => quantity;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Please, insert a quantity above 0");
+                }
+
+                quantity = value;
+            }
+        }
+    }
+}
