5d6eca95a9948277ad4d05ef1882c137c458de99
De4dSh0t <ruipsilva2001@hotmail.com>
2020-04-21 19:34:07 +0100

    QuestSystem v0.1 (Created Project and Added "Quest()")

---
+++ QuestSystem/QuestSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuestSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ QuestSystem/QuestSystem/Quest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace QuestSystem
+{
+    public class Quest
+    {
+        private string name;
+        private string description;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Please, insert a name.");
+                }
+
+                name = value;
+            }
+        }
+
+        public string Description
+        {
+            get => description;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Please, insert a description.");
+                }
+
+                description = value;
+            }
+        }
+    }
+}
