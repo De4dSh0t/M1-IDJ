74b008ffc418fcc44cd7971fac24c1ffc91044d1
De4dSh0t <ruipsilva2001@hotmail.com>
2020-04-25 17:46:39 +0100

    QuestSystem v0.4 (Added "SideQuest" class)

---
+++ QuestSystem/QuestSystem/SideQuest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace QuestSystem
+{
+    public class SideQuest : Quest
+    {
+        public SideQuest(string name, string description, TimeSpan duration) : base(name, description, duration) {}
+    }
+}
