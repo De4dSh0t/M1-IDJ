89a45cd48b230eb0f5c9ea4c69192ffeecac26c3
De4dSh0t <ruipsilva2001@hotmail.com>
2020-04-28 16:25:00 +0100

    QuestSystem 0.2 [Restart] (Removed "MainQuest" and "SideQuest")

--- QuestSystem/QuestSystem/MainQuest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace QuestSystem
-{
-    public class MainQuest : Quest
-    {
-        public MainQuest(string name, string description, TimeSpan duration) : base(name, description, duration) {}
-    }
-}
--- QuestSystem/QuestSystem/SideQuest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace QuestSystem
-{
-    public class SideQuest : Quest
-    {
-        public SideQuest(string name, string description, TimeSpan duration) : base(name, description, duration) {}
-    }
-}
